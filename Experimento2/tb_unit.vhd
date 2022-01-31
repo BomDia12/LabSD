ca487080c78e03b504796671b35fa2623f7faec8
bomdia12 <chamatico2@gmail.com>
2022-01-31 20:40:27 -0300

    ✨ Unit Sum
    added the unit circuit for thr complete sum

--- Experimento2/tb_unit.vhd
+++ Experimento2/tb_unit.vhd
@@ -1,1 +1,45 @@
+entity tb_unit is end;
 
+library ieee;
+use ieee.std_logic_1164.all;
+use std.textio.all;
+
+architecture test of tb_unit is
+
+    component unit
+    port (
+        i1, i2, i3 : in std_logic;
+        y : out std_logic
+    );
+    end component;
+
+    signal i : std_logic_vector (2 downto 0);
+
+    -- Time Constants
+    constant c : time := 20 ns;
+    constant b : time := 10 ns;
+    constant a : time := 5 ns;
+
+begin
+
+    u1 : unit port map (i(0), i(1), i(2), open);
+
+    i_0 : process
+    begin
+        i(0) <= '0', '1' after (a/2), '0' after a;
+        wait for a;
+    end process;
+
+    i_1 : process
+    begin
+        i(1) <= '0', '1' after (b/2), '0' after b;
+        wait for b;
+    end process;
+
+    i_2 : process
+    begin
+        i(2) <= '0', '1' after (c/2), '0' after c;
+        wait for c;
+    end process;
+
+end test;
--- Experimento2/unit.vhd
+++ Experimento2/unit.vhd
@@ -2,5 +2,21 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity unit is port (
-    
-)
+    i1, i2, i3 : in std_logic;
+    y: out std_logic
+);
+end unit;
+
+architecture rtl of unit is
+
+    signal s : std_logic_vector (3 downto 0);
+
+begin
+
+    s(0) <= (not i1) and (not i2) and i3;
+    s(1) <= (not i1) and i2 and (not i3);
+    s(2) <= i1 and (not i2) and (not i3);
+    s(3) <= i1 and i2 and i3;
+    y <= s(0) or s(1) or s(2) or s(3);
+
+end rtl;
