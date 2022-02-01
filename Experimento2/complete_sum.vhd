cf6c1670fca63c276c195738bfa188dc831f5613
bomdia12 <chamatico2@gmail.com>
2022-01-31 21:09:36 -0300

    ✅ Complete sum
    Complete sum circuit done

--- Experimento2/complete_sum.vhd
+++ Experimento2/complete_sum.vhd
@@ -1,1 +1,31 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
+entity complete_sum is port (
+    i0, i1, i2 : in std_logic;
+    c, u : out std_logic
+);
+end complete_sum;
+
+architecture rtl of complete_sum is
+
+    component carry_over
+    port (
+        i1, i2, i3 : in std_logic;
+        y : out std_logic
+    );
+    end component;
+
+    component unit
+    port (
+        i1, i2, i3 : in std_logic;
+        y : out std_logic
+    );
+    end component;
+
+begin
+
+    carry: carry_over port map (i0, i1, i2, c);
+    un   : unit       port map (i0, i1, i2, u);
+
+end rtl;
