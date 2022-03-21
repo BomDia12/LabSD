49407a18402f34f140f253062ae41a48049b3d5b
Arthur Mota Furtado <bomdia@pop-os.localdomain>
2022-03-21 10:43:56 -0300

    questao 2 exp 6

---
+++ Experimento6/registrador.vhd
@@ -0,0 +1,33 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity registry is port (
+    d : in std_logic_vector (3 downto 0);
+    l, r, dir, load, rst, clk : in std_logic;
+    q : out std_logic_vector (3 downto 0)
+);
+end registry;
+
+architecture arch of registry is
+
+    signal aux : std_logic_vector (3 downto 0);
+
+begin
+
+    process (clk)
+        if rising_edge(clk) then
+            if rst = '1' then aux <= "0000";
+            elsif load = '1' then aux <= d;
+            elsif dir = '0' then
+                aux <= aux(2) & aux(1) & aux(0) & l;
+            elsif dir = '1' then
+                aux <= r & aux(3) & aux(2) & aux(1);
+            end if;
+        else
+            aux <= aux;
+        end if;
+    end process;
+
+    q <= aux;
+
+end arch;
